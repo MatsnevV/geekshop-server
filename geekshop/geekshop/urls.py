0fbd1b27f87fbfac983064402af71cc6fdc318f7
vmats <v.matsnev@yandex.ru>
2021-11-16 22:31:16 +0300

    push1

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, products, contact
+import mainapp.views as mainapp
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='name'),
-    path('products/', products, name='products'),
-    path('contact/', contact, name='contact'),
+    path('', mainapp.index, name='name'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
 ]
+#index, products, contact
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, '/index.html')
+
 
 def products(request):
-    return render(request, 'products.html')
+    return render(request, '/products.html')
+
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, '/contact.html')
 
 
 
